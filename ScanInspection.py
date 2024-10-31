d993bc8539cfab1259aa4121a744a7586b86e25f
bluezealot <qin_xizhou@hoperun.com>
2024-10-31 14:15:14 +0800

    Add implement.

--- ScanInspection.py
+++ ScanInspection.py
@@ -42,51 +42,66 @@ def caluclate_timespan(start_time, end_time):
 
 with open("output_outInspect.csv", "w") as file_out:
     with open("output_inspect.csv", "w") as file_in:
-        for file_name in os.listdir(folder_path):
-            if file_name.endswith('.log') and file_name.startswith('Robot_info'):
-                file_path = os.path.join(folder_path, file_name)
-                with open(file_path, 'r') as file_read:
-                    line = file_read.readline()
-                    start_time = ''
-                    instart_time = ''
-                    while line:
-                        print(line.strip())
-                        spend_time = str(0)
-                        inspend_time = str(0)
-                        if line.__contains__('OutSideInspectionActivity') and line.__contains__('InspectionActivity onCreate start'):
-                            matches = re.findall(datetime_pattern, line)
-                            if start_time:
-                                time_span = caluclate_timespan(matches[0], start_time)
-                                spend_time = str(time_span)
-                            start_time = matches[0]
-                            odata = inspection_data(line, True, matches[0], spend_time)
-                            spend_time = str(0)
-                            file_out.write(odata.to_csv())
-                        if line.__contains__('OutSideInspectionActivity') and line.__contains__('InspectionActivity stopNav'):
-                            matches = re.findall(datetime_pattern, line)
-                            if start_time:
-                                time_span = caluclate_timespan(matches[0], start_time)
-                                spend_time = str(time_span)
-                            odata = inspection_data(line, False, matches[0], spend_time)
-                            start_time = ''
-                            spend_time = str(0)
-                            file_out.write(odata.to_csv())
-                        if line.__contains__('InspectionActivity') and line.__contains__('InspectionActivity onCreate start'):
-                            matches = re.findall(datetime_pattern, line)
-                            if instart_time:
-                                time_span = caluclate_timespan(matches[0], instart_time)
-                                inspend_time = str(time_span)
-                            instart_time = matches[0]
-                            odata = inspection_data(line, True, matches[0], inspend_time)
-                            inspend_time = str(0)
-                            file_in.write(odata.to_csv())
-                        if line.__contains__('InspectionActivity') and line.__contains__('InspectionActivity stopNav'):
-                            matches = re.findall(datetime_pattern, line)
-                            if instart_time:
-                                time_span = caluclate_timespan(matches[0], instart_time)
-                                spend_time = str(time_span)
-                            odata = inspection_data(line, False, matches[0], inspend_time)
-                            instart_time = ''
-                            inspend_time = str(0)
-                            file_in.write(odata.to_csv())
+        with open("output_outInspectCall.csv", "w") as file_out1:
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith('.log') and file_name.startswith('Robot_info'):
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, 'r') as file_read:
                         line = file_read.readline()
+                        start_time = ''
+                        instart_time = ''
+                        while line:
+                            print(line.strip())
+                            spend_time = str(0)
+                            inspend_time = str(0)
+                            if line.__contains__('OutSideInspectionActivity') and line.__contains__('InspectionActivity onCreate start'):
+                                matches = re.findall(datetime_pattern, line)
+                                time_span = 0
+                                if start_time:
+                                    time = caluclate_timespan(start_time, matches[0])
+                                    time_span = time.total_seconds()
+                                start_time = matches[0]
+                                odata = inspection_data(line, True, matches[0], str(time_span))
+                                file_out.write(odata.to_csv())
+                            if line.__contains__('OutSideInspectionActivity') and line.__contains__('InspectionActivity stopNav'):
+                                matches = re.findall(datetime_pattern, line)
+                                time_span = 0
+                                if start_time:
+                                    time = caluclate_timespan(start_time, matches[0])
+                                    time_span = time.total_seconds()
+                                odata = inspection_data(line, False, matches[0], str(time_span))
+                                start_time = ''
+                                spend_time = str(0)
+                                file_out.write(odata.to_csv())
+                            if not line.__contains__('OutSideInspectionActivity') and line.__contains__('InspectionActivity onCreate start'):
+                                matches = re.findall(datetime_pattern, line)
+                                time_span = 0
+                                if instart_time:
+                                    time = caluclate_timespan(instart_time, matches[0])
+                                    time_span = time.total_seconds()
+                                instart_time = matches[0]
+                                odata = inspection_data(line, True, matches[0], str(time_span))
+                                inspend_time = str(0)
+                                file_in.write(odata.to_csv())
+                            if not line.__contains__('OutSideInspectionActivity') and line.__contains__('InspectionActivity stopNav'):
+                                matches = re.findall(datetime_pattern, line)
+                                time_span = 0
+                                if instart_time:
+                                    time = caluclate_timespan(instart_time, matches[0])
+                                    time_span = time.total_seconds()
+                                odata = inspection_data(line, False, matches[0], str(time_span))
+                                instart_time = ''
+                                inspend_time = str(0)
+                                file_in.write(odata.to_csv())
+                            line = file_read.readline()
+                if file_name.endswith('.log') and file_name.startswith('SPEECH'):
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, 'r') as file_read:
+                        line = file_read.readline()
+                        while line:
+                            if line.__contains__('私は名鉄商店ロボットのショウです。') or line.__contains__(
+                                '私は名鉄商店のロボットのショウです。'):
+                                matches = re.findall(datetime_pattern, line)
+                                odata = inspection_data(line, False, matches[0], str(1))
+                                file_out1.write(odata.to_csv())
+                            line = file_read.readline()
